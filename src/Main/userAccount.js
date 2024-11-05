0b6aa2a1bd06294efc3c1b3f8a551aeb730e0efc
manideep Reddy <manideepreddy170400@gmail.com>
2024-11-05 13:44:15 +0330

    cssChanges

--- src/Main/userAccount.js
+++ src/Main/userAccount.js
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import travelHistory from "../travelData";
+import "../styles/all-users.css";
 
 function UserAccount() {
   const navigate = useNavigate();
   const [users, setUsers] = useState([]);
+  let [show, setShow] = useState(false);
 
   const navigateToAllUsers = () => {
     navigate("/all-users");
@@ -18,7 +20,6 @@ function UserAccount() {
     navigate("/update");
   };
 
-  let usersContent;
   const travelPlace = travelHistory(navigate);
 
   const pathObject = {
@@ -28,18 +29,32 @@ function UserAccount() {
     authLogin: authLoginPage,
   };
 
+  const showAndHide = () => {
+    setShow(!show);
+  };
+
+  let usersContent;
   if (!users || users.length === 0) {
     usersContent = <p>No users found</p>;
   } else {
     usersContent = users.map((user, index) => (
-      <div key={index}>
-        <p>{user.title}</p>
+      <div key={index} style={{ display: "flex" }}>
         <img
           src={`http://localhost:5000/${user.images[0]}`}
           alt={user.title}
-          style={{ width: "50px" }}
+          style={{ width: "50px", height: "100px" }}
         />
-        <p>{user.description}</p>
+        <div>
+          <span className="material-symbols-outlined" onClick={showAndHide}>
+            arrow_drop_down
+          </span>
+          {show && (
+            <div>
+              <p>{user.title}</p>
+              <p>{user.description}</p>
+            </div>
+          )}
+        </div>
       </div>
     ));
   }
@@ -56,10 +71,7 @@ function UserAccount() {
         <button onClick={pathObject.authLogin}>AUTHENTICATION</button>
         <button onClick={pathObject.update}>Update</button>
       </div>
-
-      <div>
-        <div>{usersContent}</div>
-      </div>
+      <div className="userContent">{usersContent}</div>
     </div>
   );
 }
